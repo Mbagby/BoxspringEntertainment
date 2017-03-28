cc77f45ac16db4f5e43e38adba0475f328b847ee
webmaster219 <webmaster219@gmail.com>
2017-03-28 23:32:15 +0300

    HR Manager can Disable the link to the message boards and hide the comments section. Please make two items in the permissions where they can turn this aspect on or off

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,6 +3,7 @@ class EpisodesController < ApplicationController
 	def show
 		# @categories = Category.order('category_id').all
 		@episode = Episode.find_by_id(params[:id])
+		@category = @episode.category
 		@quizes = Quiz.all
 		@user = User.find_by_id(session[:user_id])
 	end
