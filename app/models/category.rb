ea7eab5d6f15deea31297cd7f43ee7760707b711
Ankur M <amsystango@gmail.com>
2017-04-20 12:42:34 +0530

    Changed back navbar.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,44 +2,44 @@
 #
 # Table name: categories
 #
-# id 							:integer not null, primary key
-# category_id 		:integer
-# title  					:string
-# description 		:text
-# icon 						:string
-# banner 					:string
-# snap_shot 			:string
+# id              :integer not null, primary key
+# category_id     :integer
+# title           :string
+# description     :text
+# icon            :string
+# banner          :string
+# snap_shot       :string
 #
-# created_at 			:datetime
-# updated_at 			:datetime
+# created_at      :datetime
+# updated_at      :datetime
 class Category < ActiveRecord::Base
-	has_many :topics, dependent: :destroy
-	has_many :seasons, dependent: :destroy
-	has_many :episodes, dependent: :destroy
-	has_many :messages, as: :messageable
-	has_many :asset_libraries
-	has_many :category_options
+  has_many :topics, dependent: :destroy
+  has_many :seasons, dependent: :destroy
+  has_many :episodes, dependent: :destroy
+  has_many :messages, as: :messageable
+  has_many :asset_libraries
+  has_many :category_options
 
-	mount_uploader :snap_shot, SnapShotUploader
-	mount_uploader :banner, BannerUploader
+  mount_uploader :snap_shot, SnapShotUploader
+  mount_uploader :banner, BannerUploader
 
-	scope :most_recent, -> (limit) { order("created_at desc").limit(limit) }
-	def category_option user
-		category_options.find_by(user_id:user.id)
-	end
+  scope :most_recent, -> (limit) { order("created_at desc").limit(limit) }
+  def category_option user
+    category_options.find_by(user_id:user.id)
+  end
 
-	def message_board_permission user
-		return true if user.single_user?
-		user = user.hr_manager unless user.hr_manager?
-		cat_option = category_options.find_by(user_id:user.id)
-		cat_option.present? and cat_option.message_board == true
-	end
+  def message_board_permission user
+    return true if user.single_user?
+    user = user.hr_manager unless user.hr_manager?
+    cat_option = category_options.find_by(user_id:user.id)
+    cat_option.present? and cat_option.message_board == true
+  end
 
-	def comment_section_permission user
-		return true if user.single_user?
-		user = user.hr_manager unless user.hr_manager?
-		cat_option = category_options.find_by(user_id:user.id)
-		cat_option.present? and cat_option.comment_section == true
-	end
+  def comment_section_permission user
+    return true if user.single_user?
+    user = user.hr_manager unless user.hr_manager?
+    cat_option = category_options.find_by(user_id:user.id)
+    cat_option.present? and cat_option.comment_section == true
+  end
 
 end
